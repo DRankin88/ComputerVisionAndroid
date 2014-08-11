5672233c1f39dbb1c8edb3632d316ddfc9c83958
davidrankin <rankin1988@gmail.com>
2014-08-11 01:05:41 +0100

    Basic funtionality with some advanced

--- app/src/main/java/spaceproblems/camerathingy/DisplayCamera.java
+++ app/src/main/java/spaceproblems/camerathingy/DisplayCamera.java
@@ -4,20 +4,39 @@ import spaceproblems.camerathingy.utils.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-
-//import spaceproblems.camerathingy.camera.CameraView;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 /**
@@ -37,7 +56,9 @@ public class DisplayCamera extends Activity implements SurfaceHolder.Callback {
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
      * user interaction before hiding the system UI.
      */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 1000;
+    private static final int AUTO_HIDE_DELAY_MILLIS = 1500;
+
+    private static final String TAG = "DisplayCamera";
 
     /**
      * If set, will toggle the system UI visibility upon interaction. Otherwise,
@@ -54,12 +75,17 @@ public class DisplayCamera extends Activity implements SurfaceHolder.Callback {
      * The instance of the {@link SystemUiHider} for this activity.
      */
     private SystemUiHider mSystemUiHider;
-
-    private Camera camera;
+    private SurfaceView mSurfaceView;
+    private ImageView mImageView;
+    private Camera.PictureCallback mPicture;
+    //    private Preview mPreview;
+    private Timer timer;
+    private File pictureFile;
+    private Camera mCamera;
+    private boolean capturing = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_display_camera);
@@ -68,6 +94,7 @@ public class DisplayCamera extends Activity implements SurfaceHolder.Callback {
         final View controlsView = findViewById(R.id.fullscreen_content_controls);
 //        final View contentView = findViewById(R.id.fullscreen_content);
         final View contentView = findViewById(R.id.mySurfaceView);
+
 
         // Set up an instance of SystemUiHider to control the system UI for
         // this activity.
@@ -126,58 +153,95 @@ public class DisplayCamera extends Activity implements SurfaceHolder.Callback {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.startCapturing).setOnTouchListener(mDelayHideTouchListener);
 
-        camera = getCameraInstance();
-        Camera.Parameters parameters = camera.getParameters();
-        SurfaceView surfaceView = (SurfaceView)findViewById(R.id.mySurfaceView);
-        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        mSurfaceView = (SurfaceView) findViewById(R.id.mySurfaceView);
+        SurfaceHolder surfaceHolder = mSurfaceView.getHolder();
         surfaceHolder.addCallback(this);
-//        CameraView myCameraView = new CameraView(getApplicationContext(), camera);
-//        setContentView(myCameraView);
-        try {
-            camera.setPreviewDisplay(surfaceHolder);
-            camera.startPreview();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        mPicture = new Camera.PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+
+                pictureFile = getOutputMediaFile(1);
+                if (pictureFile == null) {
+                    Log.d(TAG, "Error creating media file, check storage permissions: ");
+                    return;
+                }
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    data = greyScaledImage();
+                    fos.write(data);
+                    fos.close();
+                    displayToast(pictureFile);
+                } catch (FileNotFoundException e) {
+                    Log.d(TAG, "File not found: " + e.getMessage());
+                } catch (IOException e) {
+                    Log.d(TAG, "Error accessing file: " + e.getMessage());
+                }
+            }
+        };
+
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width,
                                int height) {
     }
+
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
 
-        try {
-            camera.setPreviewDisplay(holder);
-        } catch (IOException e) {
-            camera.release();
+        if (safeCameraOpen()) {
+            mCamera.setDisplayOrientation(90);
+            Camera.Parameters parameters = mCamera.getParameters();
+
+            try {
+                mCamera.setPreviewDisplay(holder);
+            } catch (IOException e) {
+                mCamera.release();
+            }
+
+            parameters.set("orientation", "portrait");
+            mCamera.setParameters(parameters);
+            mCamera.startPreview();
         }
-        camera.startPreview();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
 
-        camera.stopPreview();
-        camera.release();
-
+        // Surface will be destroyed when we return, so stop the preview.
+        if (mCamera != null) {
+            // Call stopPreview() to stop updating the preview surface.
+            mCamera.stopPreview();
+            releaseCameraAndPreview();
+        }
     }
 
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance()
-    {
-        Camera c = null;
+    private boolean safeCameraOpen() {
+        boolean qOpened = false;
+
         try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
+            releaseCameraAndPreview();
+            mCamera = Camera.open();
+            qOpened = (mCamera != null);
+        } catch (Exception e) {
+            Log.e(getString(R.string.app_name), "failed to open Camera");
             e.printStackTrace();
         }
-        return c; // returns null if camera is unavailable
+
+        return qOpened;
+    }
+
+    private void releaseCameraAndPreview() {
+
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
     }
 
     @Override
@@ -251,4 +315,109 @@ public class DisplayCamera extends Activity implements SurfaceHolder.Callback {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
+
+    public void startCapturing(View view) {
+
+        if (capturing == true) {
+            capturing = false;
+            timer.cancel();
+            return;
+        } else if (capturing == false) {
+            capturing = true;
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    checkDirectorySizeAndPurge();
+                    mCamera.startPreview();
+                    mCamera.takePicture(null, null, mPicture);
+                }
+            }, 0, 2000);
+        }
+    }
+
+    private void displayToast(File picture) {
+
+        Context context = getApplicationContext();
+        long fileSize = picture.length();
+        CharSequence text = "Size of File Saved " + fileSize;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    private void purgeOldestFile(File directory) {
+
+        File[] files = directory.listFiles();
+
+        Arrays.sort(files, new Comparator<File>() {
+            public int compare(File f1, File f2) {
+                return Long.valueOf(f1.lastModified()).compareTo(f2.lastModified());
+            }
+        });
+        Log.d(TAG, "deleting file " + files[0].getName());
+        files[0].delete();
+
+    }
+
+    private void checkDirectorySizeAndPurge() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraAppThingy");
+        long size = getFolderSize(mediaStorageDir);
+        while (size > 2000000) {
+            Log.d(TAG, "PURGING DIRECTORY");
+            purgeOldestFile(mediaStorageDir);
+            size = getFolderSize(mediaStorageDir);
+        }
+    }
+
+    public long getFolderSize(File dir) {
+        long size = 0;
+        for (File file : dir.listFiles()) {
+            size += file.length();
+        }
+        Log.d(TAG, "Folder size " + size);
+        return size;
+    }
+
+
+    /**
+     * Create a File for saving an image or video
+     */
+    private static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraAppThingy");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraAppThingy", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == 1) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    private byte[] greyScaledImage(byte[] data){
+
+
+
+    }
+
 }
